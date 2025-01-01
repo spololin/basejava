ba12c639a8a7e5642f3be07b42d8b33b7d30e42b
Пололин Сергей Александрович <spololin@sportmaster.ru>
2025-01-01 22:54:55 +0300

    Выполнил задание HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,59 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume == null) {
+                break;
+            }
+            if (resume.uuid.equals(uuid)) {
+                return resume;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int idx = -1;
+        for (int i = 0; i < storage.length; i++) {
+            if (this.storage[i] == null) {
+                continue;
+            }
+            if (storage[i].uuid.equals(uuid)) {
+                idx = i;
+            }
+        }
+
+        if (idx != -1) {
+            for (int i = idx + 1; i < size(); i++) {
+                storage[i - 1] = storage[i];
+            }
+            storage[size() - 1] = null;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size());
     }
 
     int size() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
         return 0;
     }
 }
